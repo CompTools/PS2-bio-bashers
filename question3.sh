dd25a6f78dfbc41c7eafe07a48c8e98085c53917
Sarah Kurtis <sarahkurtis@login2.ufhpc>
2018-09-17 10:23:08 -0400

    I added code to pull out the names of the airport codes and states

--- question3.sh
+++ question3.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 # Question 3 code
+
+head flights.May2017-Apr2018.csv | cut -d',' -f3,6,7,10 > airportcodesstates.csv
+cut -d',' -f1 airportcodesstates.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' ;
+cut -d',' -f3 airportcodesstates.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' > originanddest.csv
+uniq originanddest.csv
+# I can't find a way to print a list of airport codes with no duplicates. I'll keep working at it.
