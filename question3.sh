6fe9232cbae7520f6417ca15412c6d8c5dd0994a
skurtis <42898516+skurtis@users.noreply.github.com>
2018-09-19 17:53:22 -0400

    Update question3.sh
    Question 3 is finished!

--- question3.sh
+++ question3.sh
@@ -3,7 +3,8 @@
 # Question 3 code
 
 cut -d',' -f1 HW2_dataset.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' > originanddest.txt
+# The above line isolates the column of origin airport codes and outputs that to the file "originanddest.txt"
 cut -d',' -f5 HW2_dataset.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' >> originanddest.txt
-
-uniq originanddest.txt > unique_airport_codes.txt
-# uniq is still printing clear duplicates in this list of airport codes
+# The above line isolated the column of destination airport codes and adds that as output to the file "originanddest.txt"
+sort -u originanddest.txt > unique_airport_codes.txt
+# The list of airport codes in alphabetical order is outputted in the file "unique_airport_codes.txt"
