fa47464c8e325a794d33055fc90c589571e019fd
Maria Cortez <mariacortez@login4.ufhpc>
2018-09-19 19:05:39 -0400

    All the commands necessary for finding the numbers of each column

--- question2.sh
+++ question2.sh
@@ -13,3 +13,18 @@ grep -E '^"GNV"' E2.csv > E2col1.csv
 #by destination:
 
 sort --field-separator="," -k2 E2col1.csv
+
+#fourth command selects the number of delayed flights by 15 minutes or more
+#for each of the 3 destinations:
+
+cat E2col1.csv | grep "ATL" | cut -d',' -f3 | grep -w "1.00" -c
+cat E2col1.csv | grep "CLT" | cut -d',' -f3 | grep -w "1.00" -c
+cat E2col1.csv | grep "MIA" | cut -d',' -f3 | grep -w "1.00" -c
+
+#fifth command selects number of delayed flights due to weather
+
+cat E2col1.csv | grep "ATL" | cut -d',' -f5 | grep '^\S*' | grep -v 0.00 | grep -cE '[[:digit:]]{1,}'
+cat E2col1.csv | grep "CLT" | cut -d',' -f5 | grep '^\S*' | grep -v 0.00 | grep -cE '[[:digit:]]{1,}'
+cat E2col1.csv | grep "MIA" | cut -d',' -f5 | grep '^\S*' | grep -v 0.00 | grep -cE '[[:digit:]]{1,}'
+
+
