7b676cc4099fcbb2d52c21ad3fccae9fef7574e1
skurtis <42898516+skurtis@users.noreply.github.com>
2018-09-19 13:38:36 -0400

    Update question4.sh
    I wrote a code to pull out all the Florida cities, yet there are still issues with removing duplicates.

--- question4.sh
+++ question4.sh
@@ -2,7 +2,8 @@
 
 # Question 4 code
 
-cat airportcodesstates.csv | grep \"FL\" | grep -oE '\"[[:alpha:]][[:alpha:]][[:alpha:]]\",\"FL\"' > allFLairports.csv
-# cut -d',' -f1 allFLairports.csv | uniq # This outputs around 400K lines - that's why I commented it out for now
-# I'm having the same issue as with question 3 as to output the aairport codes without duplicates.
+cat HW2_dataset.csv | grep -oE '[[:alpha:]]{1,}, FL' | uniq > Total_Florida_airports2.txt 
+# The above command only pulls out Florida cities with airports. 
+# But I'm still struggling with finding a way to print a list with no duplicate cities using uniq. 
+# This is the same issue I've had with Question 3.
 
