990901c9998d976b28302b761c89eef134c1593e
Niyomi Wijewardena <nwijewardena@login3.ufhpc>
2018-09-19 16:57:16 -0400

    Solution to Problem 1 added

--- HW2_final.sh
+++ HW2_final.sh
@@ -2,4 +2,8 @@
 
 #Homework 2 final code
 
+#Creating file to be used in subsequent questions
 cat flights.May2017-Apr2018.csv | cut -d',' -f3,4,5,6,7,8,9,10,13,16,21,24 > HW2_dataset.csv 
+
+#Problem 1 solution
+cat HW2_dataset.csv | grep "GNV" | grep -c "1.00"
