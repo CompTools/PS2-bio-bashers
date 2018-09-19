fefe945f55140de99aabf7ca0342e575c63ca3ad
Sarah Kurtis <sarahkurtis@login4.ufhpc>
2018-09-19 12:08:00 -0400

    Fixed error with adding data to file

--- question3.sh
+++ question3.sh
@@ -3,7 +3,7 @@
 # Question 3 code
 
 cat flights.May2017-Apr2018.csv | cut -d',' -f3,6,7,10 > airportcodesstates.csv
-cut -d',' -f1 airportcodesstates.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' ;
-cut -d',' -f3 airportcodesstates.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' > originanddest.csv
+cut -d',' -f1 airportcodesstates.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' > originanddest.csv
+cut -d',' -f3 airportcodesstates.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' >> originanddest.csv
 # uniq originanddest.csv
 # I can't find a way to print a list of airport codes with no duplicates. I'll keep working at it.
