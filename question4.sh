861b9c7939c453aa5c8a251f48ce29ec17850ffc
skurtis <42898516+skurtis@users.noreply.github.com>
2018-09-19 18:19:54 -0400

    Update question4.sh
    Question 4 is complete!

--- question4.sh
+++ question4.sh
@@ -2,8 +2,5 @@
 
 # Question 4 code
 
-cat HW2_dataset.csv | grep -oE '[[:alpha:]]{1,}, FL' | uniq > Total_Florida_airports2.txt 
-# The above command only pulls out Florida cities with airports. 
-# But I'm still struggling with finding a way to print a list with no duplicate cities using uniq. 
-# This is the same issue I've had with Question 3.
-
+cat HW2_dataset.csv | grep -oE '[[:alpha:]]{1,}, FL' | sort -u > Unique_FL_airports.txt 
+# The above command uses grep to only pull out Florida cities with airports and print a list of all airports in alphabetical order.
