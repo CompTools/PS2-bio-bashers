d4b6c59cbd1348a6594fcd880e61aa0e7b22aa40
Sarah Kurtis <sarahkurtis@login3.ufhpc>
2018-09-19 12:23:04 -0400

    Fixed error with compiling airport codes

--- question3.sh
+++ question3.sh
@@ -2,8 +2,9 @@
 
 # Question 3 code
 
-cat flights.May2017-Apr2018.csv | cut -d',' -f3,6,7,10 > airportcodesstates.csv
+cat flights.May2017-Apr2018.csv | cut -d',' -f3,4,6,7,8,10 > airportcodesstates.csv
 cut -d',' -f1 airportcodesstates.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' > originanddest.csv
-cut -d',' -f3 airportcodesstates.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' >> originanddest.csv
+cut -d',' -f4 airportcodesstates.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' >> originanddest.csv
+
 # uniq originanddest.csv
 # I can't find a way to print a list of airport codes with no duplicates. I'll keep working at it.
