24e48ba3a4c3a1e12cc446733c46836a41021b1d
skurtis <42898516+skurtis@users.noreply.github.com>
2018-09-21 08:37:59 -0400

    Update HW2_final.sh
    Added Question 4 solution

--- HW2_final.sh
+++ HW2_final.sh
@@ -15,3 +15,7 @@ cut -d',' -f5 HW2_dataset.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:
 # The above line isolated the column of destination airport codes and adds that as output to the file "originanddest.txt"
 sort -u originanddest.txt > unique_airport_codes.txt
 # The list of airport codes in alphabetical order is outputted in the file "unique_airport_codes.txt"
+
+# Question 4 solution
+cat HW2_dataset.csv | grep -oE '[[:alpha:]]{1,}, FL' | sort -u > Unique_FL_airports.txt 
+# The above command uses grep to only pull out Florida cities with airports and print a list of all airports in alphabetical order.
