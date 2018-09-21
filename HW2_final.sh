fa38302ebb84928e2a70d8f0cc40a2fa4f9afea1
Sarah Kurtis <sarahkurtis@login4.ufhpc>
2018-09-21 10:05:57 -0400

    Fixed small error with calling file in question 1

--- HW2_final.sh
+++ HW2_final.sh
@@ -6,7 +6,7 @@
 cat flights.May2017-Apr2018.csv | cut -d',' -f3,4,5,6,7,8,9,10,13,16,21,24 > HW2_dataset.csv 
 
 #Problem 1 solution
-cat HW2_dataset.csv | grep "GNV" | grep -c "1.00"
+cat HW2_dataset.csv | cut -d',' -f1,2,3,4,5,6,9,10 | grep "GNV" | grep -c "1.00"
 
 # Problem 3 Solution
 question3_function () {
