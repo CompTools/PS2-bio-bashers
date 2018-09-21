6dc5f80511552ba05f651dc2c1094e6f734d0cb0
skurtis <42898516+skurtis@users.noreply.github.com>
2018-09-21 11:04:42 -0400

    Update HW2_final.sh
    Added indents to questions 3 and 4 functions

--- HW2_final.sh
+++ HW2_final.sh
@@ -73,22 +73,22 @@ cat header.txt
 
 #Problem 3 Solution
 question3_function () {
-cut -d',' -f1 HW2_dataset.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' > originanddest.txt
-# The above line isolates the column of origin airport codes and outputs that to the file "originanddest.txt"
-cut -d',' -f5 HW2_dataset.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' >> originanddest.txt
-# The above line isolated the column of destination airport codes and adds that as output to the file "originanddest.txt"
-sort -u originanddest.txt > unique_airport_codes.txt
-cat unique_airport_codes.txt
-# The list of airport codes in alphabetical order is outputted in the file "unique_airport_codes.txt"
+  cut -d',' -f1 HW2_dataset.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' > originanddest.txt
+  # The above line isolates the column of origin airport codes and outputs that to the file "originanddest.txt"
+  cut -d',' -f5 HW2_dataset.csv | uniq | grep -E '"[[:alpha:]][[:alpha:]][[:alpha:]]"' >> originanddest.txt
+  # The above line isolated the column of destination airport codes and adds that as output to the file "originanddest.txt"
+  sort -u originanddest.txt > unique_airport_codes.txt
+  cat unique_airport_codes.txt
+  # The list of airport codes in alphabetical order is outputted in the file "unique_airport_codes.txt"
 }
 question3_function 
-# The above line executes the function within the bash script and thus produces the list of airport codes in the file "uniqure_airport_codes.txt"
+# The above line executes the function within the bash script and thus produces the list of airport codes in the file "unique_airport_codes.txt"
 
 # Problem 4 solution
 question4_function () {
-cat HW2_dataset.csv | grep -oE '[[:alpha:]]{1,}, FL' | sort -u > Unique_FL_airports.txt 
-cat Unique_FL_airports.txt
-# The above command uses grep to only pull out Florida cities with airports and print a list of all airports in alphabetical order.
+  cat HW2_dataset.csv | grep -oE '[[:alpha:]]{1,}, FL' | sort -u > Unique_FL_airports.txt 
+  cat Unique_FL_airports.txt
+  # The above command uses grep to only pull out Florida cities with airports and print a list of all airports in alphabetical order.
 }
 question4_function
 # The above line executes the function within the bash script and creates a list of airport codes in the file "Unique_FL_airports.txt"
